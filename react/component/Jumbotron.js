b1dd8068de5d11a59b3bebd129d9bf74273a20ef
natchanonBenz <natchanon.say@gmail.com>
2021-04-25 01:13:06 +0700

    Create Jumbotron.js

---
+++ react/component/Jumbotron.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Jumbotron() {
+    return (
+        <div>
+            <div class="jumbotron jumbotron-fluid-sm">
+                <div class="container">
+                    <h1 class="display-8">If you can't hold, you won't be rich.</h1>
+                    <p class="lead">Author: Changpeng Zhao</p>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default Jumbotron

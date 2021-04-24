87dc5050611286a4c93e5d3a5cdec63e1433e185
natchanonBenz <natchanon.say@gmail.com>
2021-04-25 01:12:51 +0700

    Create Footer.js

---
+++ react/component/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div class="jumbotron jumbotron-fluid" style={{ marginTop: "3%" }}>
+        </div>
+    );
+
+}
+
+export default Footer
